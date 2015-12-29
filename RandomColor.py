67d03c1f4696151602ba9d1369438e986f0d7623
Mohammed Zaher <moh.zaher.coder@gmail.com>
2015-12-29 14:34:18 +0300

    commiting RandomColor.py

---
+++ RandomColor.py
@@ -0,0 +1,6 @@
+import random #import random methods
+#Creat list of total hexdigits for colors
+letters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F']
+hexColor = random.sample(letters, 6) # a variable that creat random 6 numbers from the letters list
+color = ''.join(hexColor) #remove the spaces
+print('#'+color)
