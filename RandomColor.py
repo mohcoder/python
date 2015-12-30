cee82729ff5f0fedde4186e2374519f9a27f37e7
Mohammed Zaher <moh.zaher.coder@gmail.com>
2015-12-30 15:55:34 +0300

    add more scientific calculations using class inheritance

--- RandomColor.py
+++ RandomColor.py
@@ -1,6 +1,8 @@
 import random #import random methods
 #Creat list of total hexdigits for colors
 letters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F']
-hexColor = random.sample(letters, 6) # a variable that creat random 6 numbers from the letters list
-color = ''.join(hexColor) #remove the spaces
-print('#'+color)
+ColorGen = random.sample(letters, 6) # a variable that creat random 6 numbers from the letters list
+Color = ''.join(ColorGen) #remove the spaces
+hexColor = ('#'+Color)
+
+print(hexColor)
---
+++ ScientificCalc.py
@@ -0,0 +1,50 @@
+import math
+x = int(input('num1: '))
+y = int(input('num2: '))
+
+
+class Calc:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def add(self):
+		return self.x + self.y
+
+	def multiple(self):
+		return self.x * self.y
+
+	def divide(self):
+		return self.x / self.y
+
+	def subs(self):
+		return self.x - self.y
+
+
+class ScientificCalc(Calc):
+	def power(self):
+		return pow(self.x, self.y)
+
+	def x_root(self):
+		return math.sqrt(x)
+
+	def y_root(self):
+		return math.sqrt(y)
+
+	def x_fact(self):
+		return math.factorial(x)
+
+	def y_fact(self):
+		return math.factorial(y)
+
+num = ScientificCalc(x, y)
+
+print('{} + {} = {} '.format(x, y, num.add()))
+print('{} * {} = {} '.format(x, y, num.multiple()))
+print('{} / {} = {} '.format(x, y, num.divide()))
+print('{} - {} = {} '.format(x, y, num.subs()))
+print('{} ^ {} = {} '.format(x, y, num.power()))
+print('Square of {} = {} '.format(x, num.x_root()))
+print('Square of {} = {} '.format(y, num.y_root()))
+print('Factorial number of {} = {} '.format(x, num.x_fact()))
+print('Factorial number of {} = {} '.format(y, num.y_fact()))
