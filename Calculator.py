5a5ea073e36dfbe46fa7b192fc371bf55e66b7f3
mohcoder <moh.zaher.coder@gmail.com>
2016-01-12 00:19:33 +0300

    Reformat the code using pycharm.

--- Calculator.py
+++ Calculator.py
@@ -3,23 +3,25 @@ b = int(input('num2: '))
 
 
 class Calc:
-	def __init__(self, a, b):
-			self.a = a
-			self.b = b
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-	def add(self):
-		return self.a + self.b
+    def add(self):
+        return self.a + self.b
 
-	def multiple(self):
-		return self.a * self.b
+    def multiple(self):
+        return self.a * self.b
 
-	def divide(self):
-		return self.a / self.b
+    def divide(self):
+        return self.a / self.b
 
-	def subs(self):
-		return self.a - self.b
+    def subs(self):
+        return self.a - self.b
+
+
 num = Calc(a, b)
-print('{} + {} = {} '.format(a,b,num.add()))
-print('{} * {} = {} '.format(a,b,num.multiple()))
-print('{} / {} = {} '.format(a,b,num.divide()))
-print('{} - {} = {} '.format(a,b,num.subs()))
+print('{} + {} = {} '.format(a, b, num.add()))
+print('{} * {} = {} '.format(a, b, num.multiple()))
+print('{} / {} = {} '.format(a, b, num.divide()))
+print('{} - {} = {} '.format(a, b, num.subs()))
--- RandomColor.py
+++ RandomColor.py
@@ -1,8 +1,10 @@
-### Random hex Color Generator 
-import random 
-#Creat list of total hexdigits for colors
+# Random hex Color Generator
+import random
+
+# Create list of total hexdigits for colors
+
 letters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F']
-ColorGen = random.sample(letters, 6) # a variable that creat random 6 numbers from the letters list
-Color = ''.join(ColorGen) #remove the spaces
-hexColor = ('#'+Color)
+ColorGen = random.sample(letters, 6)  # a variable that create random 6 numbers from the letters list
+Color = ''.join(ColorGen)  # remove the spaces
+hexColor = ('#' + Color)
 print(hexColor)
