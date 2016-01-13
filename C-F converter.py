53a645ae9efea92166e31828d83910773f91094b
Mohammed Zaher <moh.zaher.coder@gmail.com>
2016-01-13 13:34:34 +0300

    reformat code

--- C-F converter.py
+++ C-F converter.py
@@ -1,12 +1,16 @@
+# @ author(Mohammed Zaher)
+# @ email (moh.zaher.coder@gmail.com)
+# @ copyrights 2016
+
 temp = 0
 
 
 def tempconverter():
-    print('Welcome to Celsius - Fahrenheit Converter')
-    print('=========================================')
-    print('Please Choose "C" to convert from Fahrenheit to Celsius')
-    print('Please Choose "F" to convert from Celsius to Fahrenheit')
-    print('=========================================')
+    print('''Welcome to Python Temperature Converter
+============================================================
+|| Please Choose "C" to convert from Fahrenheit to Celsius ||
+|| Please Choose "F" to convert from Celsius to Fahrenheit ||
+============================================================''')
     choice()
 
 
@@ -23,17 +27,28 @@ def choice():
 
 def inputCheck(userChoise):
     if userChoise == 'c':
-        temp = float(input('Please, Enter the Fahrenheit temperature: '))
-        result = round((5.0 / 9.0) * (temp - 32), 2)
-        celsius = '%s \u2103' % result
-        print(celsius)
-        print('Thanks for using our converter')
+        return celsiusFunction()
     if userChoise == 'f':
+        return fahrenheitFunction()
+
+
+def fahrenheitFunction():
         temp = float(input('Please, Enter the Celsius temperature: '))
         result = round((1.8 * temp) + 32, 2)
-        fahrenheit = '%s \u2109' % result
+        fahrenheit = 'Temp. is : %s \u2109' % result
+        print('===========================')
         print(fahrenheit)
-        print('Thanks for using Python Temp Converter \u00A9')
+        print('===========================')
+        print('Thank you for using Python Temp Converter \u00A9')
 
+
+def celsiusFunction():
+    temp = float(input('Please, Enter the Fahrenheit temperature: '))
+    result = round((5.0 / 9.0) * (temp - 32), 2)
+    celsius = 'Temp. is : %s \u2103' % result
+    print('===========================')
+    print(celsius)
+    print('===========================')
+    print('Thank you for using our converter')
 
 tempconverter()
